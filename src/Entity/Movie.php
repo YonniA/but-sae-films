5a200eca2135bd68e30b81078361801d5d00d097
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-12 14:43:13 +0200

    Ajout de l'entité Movie et des Getters

---
+++ src/Entity/Movie.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Movie
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}
