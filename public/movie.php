8db3b1b0cac8e10dba20363a8de5161ea51ae409
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-14 00:08:16 +0200

    restructuration de la page movie

--- public/movie.php
+++ public/movie.php
@@ -23,20 +23,28 @@ try {
 }
 $appWebPage = new AppWebPage("Films - ".$stmt->getTitle());
 $appWebPage->appendContent('<div class="list">');
+$appWebPage->appendContent('<div class="movie-description">');
 $appWebPage->appendContent("<img src='image.php?imageId={$stmt->getPosterId()}' alt='Poster de {$stmt->getTitle()}'>");
+$appWebPage->appendContent('<div class="text">');
+$appWebPage->appendContent('<div class="text__header">');
 $appWebPage->appendContent("<p>Titre : {$stmt->getTitle()}</p>");
 $appWebPage->appendContent("<p>Date : {$stmt->getReleaseDate()}</p>");
+$appWebPage->appendContent('</div>');
 $appWebPage->appendContent("<p>Nom original : {$stmt->getOriginalTitle()}</p>");
 $appWebPage->appendContent("<p>Slogan : {$stmt->getTagline()}</p>");
 $appWebPage->appendContent("<p>Durée : {$stmt->getRuntime()} minutes</p>");
 $appWebPage->appendContent("<p>Résumé : {$stmt->getOverview()}</p>");
-$appWebPage->appendContent('<div class="list_people">');
+$appWebPage->appendContent('</div>');
+$appWebPage->appendContent('</div>');
+$appWebPage->appendContent('<div class="people-list">');
 $casts = $stmt->getCast();
 foreach ($casts as $cast) {
     $people = People::findById($cast->getPeopleId());
-    $appWebPage->appendContent("<a href='people.php?peopleId={$cast->getPeopleId()}'>"."\n");
+    $appWebPage->appendContent("<a class='people-list__people' href='people.php?peopleId={$cast->getPeopleId()}'>"."\n");
     $appWebPage->appendContent("<img src='image.php?imageId={$people->getAvatarId()} alt='{$people->getName()}'>"."\n");
+    $appWebPage->appendContent('<div class="text">');
     $appWebPage->appendContent("<p>Rôle : {$appWebPage->escapeString($cast->getRole())}</p><p>Nom : {$appWebPage->escapeString($people->getName())}</p>"."\n");
+    $appWebPage->appendContent('</div>');
     $appWebPage->appendContent("</a>");
 }
 
