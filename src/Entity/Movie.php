2442dd625c549272180fecaa01808317efe3e280
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-13 11:19:55 +0200

    ajout des attributs restants dans Movie

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -8,20 +8,67 @@ class Movie
 {
     private int $id;
     private string $name;
-
+    private int $posterId;
+    private string $originalLanguage;
+    private string $originalTitle;
+    private string $overview;
+    private string $releaseDate;
+    private int $runtime;
+    private string $tagline;
     /**
      * @return int
      */
     public function getId(): int
     {
         return $this->id;
-    }
-    /**
+    }/**
      * @return string
      */
     public function getName(): string
     {
         return $this->name;
+    }/**
+     * @return int
+     */
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }/**
+     * @return string
+     */
+    public function getOriginalLanguage(): string
+    {
+        return $this->originalLanguage;
+    }/**
+     * @return string
+     */
+    public function getOriginalTitle(): string
+    {
+        return $this->originalTitle;
+    }/**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }/**
+     * @return string
+     */
+    public function getReleaseDate(): string
+    {
+        return $this->releaseDate;
+    }/**
+     * @return int
+     */
+    public function getRuntime(): int
+    {
+        return $this->runtime;
+    }/**
+     * @return string
+     */
+    public function getTagline(): string
+    {
+        return $this->tagline;
     }
 
 
