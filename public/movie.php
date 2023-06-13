45f34dd046e41ca65137fa39b5df0194f5e98d8c
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-13 14:29:27 +0200

    ajout des vérification sur le fichier movie.php

--- public/movie.php
+++ public/movie.php
@@ -5,4 +5,18 @@ use Entity\Movie;
 use Html\AppWebPage;
 use Entity\Exception\EntityNotFoundException;
 
+if (!empty($_GET["artistId"]) && ctype_digit($_GET["artistId"])) {
+    $artistId=(int)$_GET["artistId"];
+
+} else {
+    header("location: index.php");
+    exit(302);
+}
+
+try {
+    $stmt = Movie::findById($artistId);
+} catch (EntityNotFoundException) {
+    header('HTTP/1.0 404 Not Found');
+    exit(404);
+}
 echo 'test';
