fb7d0a0d9555f22ce0519b4147a1f1a82333835c
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-14 11:12:21 +0200

    liste des films joué par l'acteur dans la page people.php

--- public/people.php
+++ public/people.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Entity\Exception\EntityNotFoundException;
+use Entity\Movie;
 use Entity\People;
 use Html\AppWebPage;
 
@@ -22,11 +23,33 @@ try {
 }
 $appWebPage = new AppWebPage("Films - {$stmt->getName()}");
 $appWebPage->appendContent('<div class="list">');
+$appWebPage->appendContent('<div class="people-description">');
 $appWebPage->appendContent("<img src='image.php?imageId={$stmt->getAvatarId()}' alt='{$stmt->getName()}'>");
+$appWebPage->appendContent('<div class="text">');
 $appWebPage->appendContent("<p>Nom : {$stmt->getName()}</p>");
 $appWebPage->appendContent("<p>Lieu de naissance : {$stmt->getPlaceOfBirth()}</p>");
 $appWebPage->appendContent("<p>Date : {$stmt->getBirthday()} - {$stmt->getDeathday()}</p>");
 $appWebPage->appendContent("<p>Biographie : {$stmt->getBiography()}</p>");
-$appWebPage->appendContent('<div class="list_movie">');
+$appWebPage->appendContent('</div>');
+$appWebPage->appendContent('</div>');
+$appWebPage->appendContent('<div class="movie-list">');
+$casts = $stmt->getCast();
+foreach ($casts as $cast) {
+    $movie = Movie::findById($cast->getMovieId());
+    $appWebPage->appendContent("<a class='movie-list__movie' href='movie.php?movieId={$cast->getMovieId()}'>"."\n");
+    $appWebPage->appendContent("<img src='image.php?imageId={$movie->getPosterId()} alt='{$movie->getTitle()}'>"."\n");
+    $appWebPage->appendContent('<div class="text">');
+    $appWebPage->appendContent("<p>Titre : {$appWebPage->escapeString($movie->getTitle())}</p><p>Date : {$appWebPage->escapeString($movie->getReleaseDate())}</p>"."\n");
+    $appWebPage->appendContent('</div>');
+    $appWebPage->appendContent("<p>Rôle de l'acteur : {$appWebPage->escapeString($cast->getRole())}</p>");
+    $appWebPage->appendContent("</a>");
+
+
+
+
+}
+
+
+
 
 echo $appWebPage->toHTML();
