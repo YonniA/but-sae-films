d04018af57635a9e5761a364da4ca8c67b4b1c57
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-06-15 02:35:44 +0200

    ajout du formulaire pour la sélection d'un genre et de l'ajout de nouveaux films

--- public/index.php
+++ public/index.php
@@ -1,13 +1,43 @@
 <?php
 
 declare(strict_types=1);
-use Database\MyPdo;
+
+use Entity\Collection\GenreCollection;
+use Entity\Collection\MovieCollection;
+use Entity\Genre;
 use Html\AppWebPage;
 
 require_once '../vendor/autoload.php';
 
+if (isset($_GET['genre']) && $_GET['genre'] !== '') {
+    $genreId = (int)$_GET['genre'];
+    $genre = Genre::findById($genreId);
+    $stmt = $genre->getMovies();
+    $title = " - {$genre->getName()}";
+} else {
+    $stmt = MovieCollection::findAll();
+    $title = '';
+}
 $appWebPage = new AppWebPage('Films');
-$stmt = (new Entity\Collection\MovieCollection())->findAll();
+$appWebPage->appendContent(
+    <<<HTML
+    <div class="navbar">
+        <form action="index.php" method="get">
+            <select name="genre">
+                <option value="">Tous les genres</option>
+HTML
+);
+$genres = GenreCollection::findAll();
+foreach ($genres as $genre) {
+    $appWebPage->appendContent("<option value='{$genre->getId()}'>{$appWebPage->escapeString($genre->getName())}</option>");
+}
+$appWebPage->appendContent(<<<HTML
+            </select>
+            <input type="submit" value="Rechercher">
+        </form>
+        <a href="movie-form.php" class="btn">Ajouter un film</a>
+    </div>  
+HTML);
 $appWebPage->appendContent('<div class="list">');
 for ($i=0;$i<count($stmt);++$i) {
     $appWebPage->appendContent("<a class='movie' href='movie.php?movieId={$stmt[$i]->getId()}'>");
